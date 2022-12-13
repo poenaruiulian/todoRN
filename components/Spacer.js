d6d32324f15d39147385251db6f3e3e39424369b
poenaruiulian <poenaruiulian2003@gmail.com>
2022-12-13 20:46:24 +0200

    Added some features as: - possibility to add tasks - possibility to delete tasks - possibility to complete a task - possibility to see all the completed tasks

---
+++ components/Spacer.js
@@ -0,0 +1,11 @@
+import {View,Text} from 'react-native-ui-lib';
+
+export default function Spacer({height}){
+
+    return(
+        <View style={{height:height}}>
+            <Text> </Text>
+        </View>
+    )
+
+}
